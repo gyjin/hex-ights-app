dc1086ba10f4d688033a4e6ae9ce1468b8c15226
Ga-Young Jin <gyjinn@gmail.com>
2023-11-08 14:12:24 -0800

    Add Button component; ensure passing of props and onPress actions

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import Button from './components/Button';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello Hexights!</Text>
       <StatusBar style="auto" />
+      <Button title={"i'm a button"}></Button>
+
     </View>
   );
 }
