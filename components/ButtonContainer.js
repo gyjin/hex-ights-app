7e570e57212639ce7a8dd4db055d567739c06b51
Ga-Young Jin <gyjinn@gmail.com>
2023-11-08 16:45:36 -0800

    Add function to create series of buttons with an array of labels

--- components/ButtonContainer.js
+++ components/ButtonContainer.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import PropTypes from 'prop-types';
+import Button from './Button'
 
 
 export default function ButtonContainer({containerName, buttonLabels}) {
   
   const makeButtonCollection = () => {
     console.log("make button collection")
+    let buttonCollection = [];
+    buttonCollection = buttonLabels.map((label, i) => {
+      return <Button title={label} key={i}></Button>
+    });
+
+    return buttonCollection;
   }
 
   return (
