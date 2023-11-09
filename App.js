f416c2dcfe0a6ccaa35b2976f6def42801e9f141
Ga-Young Jin <gyjinn@gmail.com>
2023-11-09 15:46:37 -0800

    Add background image

--- App.js
+++ App.js
@@ -1,35 +1,38 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 import ButtonContainer from './components/ButtonContainer';
 
 export default function App() {
+  const image = require('./images/bg.jpg');
+
   return (
     <View style={styles.container}>
-      <Text>Hello Hexights!</Text>
-      <StatusBar style="auto" />
-      <ButtonContainer containerName={"patterns"} buttonLabels={
-        [
-          "solid",
-          "blink",
-          "wave",
-          "fade",
-          "twinkle",
-          "run",
-          "off"
-        ]
-      }></ButtonContainer>
-      <ButtonContainer containerName={"colors"} buttonLabels={
-        {
-          "red": [20, 0, 0],
-          "orange": [20, 5, 0],
-          "yellow": [20, 12, 0],
-          "green": [0, 20, 0],
-          "blue": [0, 0, 20],
-          "purple": [12, 0, 20],
-        }
-      }></ButtonContainer>
-
+      <ImageBackground source={image} style={styles.image}>
+        <Text style={styles.header}>Hello Hexights!</Text>
+        <StatusBar style="auto" />
+        <ButtonContainer containerName={"patterns"} buttonLabels={
+          [
+            "solid",
+            "blink",
+            "wave",
+            "fade",
+            "twinkle",
+            "run",
+            "off"
+          ]
+        }></ButtonContainer>
+        <ButtonContainer containerName={"colors"} buttonLabels={
+          {
+            "red": [20, 0, 0],
+            "orange": [20, 5, 0],
+            "yellow": [20, 12, 0],
+            "green": [0, 20, 0],
+            "blue": [0, 0, 20],
+            "purple": [12, 0, 20],
+          }
+        }></ButtonContainer>
+      </ImageBackground>
     </View>
   );
 }
@@ -37,8 +40,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    width: '100%',
+    justifyContent: 'center'
+  },
+  header: {
+    height: 100,
+    fontSize: 50,
+    color: 'white',
+    backgroundColor: 'transparent',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 });
